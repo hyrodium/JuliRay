a307782346d66f4e1dbd0bff88c5fc1868dce774
Horikawa Yuto <hyrodium@gmail.com>
2019-08-13 18:51:30 +0900

    update

--- Transformation.jl
+++ Transformation.jl
@@ -28,16 +28,16 @@ function translate2pov(scaling :: Scaling)
     return "object{"*translate2pov(scaling.object)*" scale "*translate2pov(scaling.k)*"}"
 end
 
-function AffineTransform(object,A;fixedpoint=[0.0,0.0,0.0])
+function AffineTransform(object::Object,A;fixedpoint=[0.0,0.0,0.0])
     b=fixedpoint-A*fixedpoint
     return AffineTransform(object,A,b)
 end
 
-function Rotate(object,v,θ,fixedpoint=[0.0,0.0,0.0])
+function Rotate(object::Object,v,θ,fixedpoint=[0.0,0.0,0.0])
     return AffineTransform(object,rotatematrix(v,θ),fixedpoint=fixedpoint)
 end
 
-function Mirror(object,p1,p2,p3)
+function Mirror(object::Object,p1,p2,p3)
     e₃=NormalVector(p1,p2,p3)
     e₁=OrthogonalVector(e₃)
     e₂=cross(e₃,e₁)
