43a613b5ab5ed9617304617217d955804277ce1c
Horikawa Yuto <hyrodium@gmail.com>
2019-08-14 11:14:15 +0900

    add clipped_by syntax

--- CSG.jl
+++ CSG.jl
@@ -41,7 +41,20 @@ struct csgDifference <: csgObject
     objects :: Array{Object,1}
     csgDifference(objects) =
     if(length(objects) ≠ 2)
-        error("だめです")
+        error("Too many objects.")
+    elseif(objects[1] == Empty)
+        Empty
+    elseif(objects[2] == Empty)
+        objects[1]
+    else
+        new(objects)
+    end
+end
+struct csgClip <: csgObject
+    objects :: Array{Object,1}
+    csgDifference(objects) =
+    if(length(objects) ≠ 2)
+        error("Too many objects.")
     elseif(objects[1] == Empty)
         Empty
     elseif(objects[2] == Empty)
@@ -60,8 +73,11 @@ end
 function csgMerge(objects::Object...)
     return csgMerge([objects...])
 end
-function csgDifference(object1,object2)
+function csgDifference(object1::Object,object2::Object)
     return csgDifference([object1,object2])
+end
+function csgClip(object1::Object,object2::Object)
+    return csgClip([object1,object2])
 end
 
 function translate2pov(csg :: csgUnion)
@@ -75,4 +91,7 @@ function translate2pov(csg :: csgMerge)
 end
 function translate2pov(csg :: csgDifference)
     return "difference{"* *(translate2pov.(csg.objects)...)*"}"
+end
+function translate2pov(csg :: csgClip)
+    return "object{"*translate2pov(csg.objects[1])*"clipped_by{"*translate2pov(csg.objects[2])*"}}"
 end
