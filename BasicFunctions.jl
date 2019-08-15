46ff7a5945b6b10116dc10e88df9d5f58b1dcf88
Horikawa Yuto <hyrodium@gmail.com>
2019-08-15 16:40:17 +0900

    cheange AbstractVector to RealVector

--- BasicFunctions.jl
+++ BasicFunctions.jl
@@ -1,5 +1,7 @@
 ## Basic functions
 
+RealVector=Union{Array{T,1} where T <: Real}
+
 function DeleteDuplicates(v::Array{T,1})::Array{T,1} where T <: Any
     w=Array{eltype(v),1}()
     if (length(v)>0)
@@ -12,7 +14,7 @@ function DeleteDuplicates(v::Array{T,1})::Array{T,1} where T <: Any
     return w
 end
 
-function OrthogonalVector(v::Array{T,1}) where T <: Real
+function OrthogonalVector(v::RealVector) where T <: Real
     if(norm(v)==0)
         error("zero vector")
     else
@@ -30,13 +32,13 @@ function isorthogonal(M::AbstractMatrix{T}) where T<:Real
     return M*M'≈oneunit(M)
 end
 
-function rotatematrix(v::AbstractVector,θ::Real)
+function rotatematrix(v::RealVector,θ::Real)
     n=normalize(v)
     N=[0 -n[3] n[2];n[3] 0 -n[1];-n[2] n[1] 0]
     return (oneunit(N)+N^2+(-cos(θ)*N^2+sin(θ)*N))
 end
 
-function Circumcenter(point₁::AbstractVector,point₂::AbstractVector,point₃::AbstractVector)
+function Circumcenter(point₁::RealVector,point₂::RealVector,point₃::RealVector)
     l₁²=(norm(point₂-point₃))^2
     l₂²=(norm(point₃-point₁))^2
     l₃²=(norm(point₁-point₂))^2
@@ -47,6 +49,6 @@ function Circumcenter(point₁::AbstractVector,point₂::AbstractVector,point₃
     return center
 end
 
-function NormalVector(p₁::AbstractVector,p₂::AbstractVector,p₃::AbstractVector)
+function NormalVector(p₁::RealVector,p₂::RealVector,p₃::RealVector)
     return normalize(cross(p₁,p₂)+cross(p₂,p₃)+cross(p₃,p₁))
 end
